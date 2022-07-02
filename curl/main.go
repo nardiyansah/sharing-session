dccf79d8c434f0fbd311b9f91c9dd9bc8fdc24b6
nardiyansah <janatri1398@gmail.com>
2022-07-02 14:19:46 +0700

    simple POST endpoint

--- curl/main.go
+++ curl/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
@@ -9,6 +10,20 @@ func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "hello world\n")
+	})
+
+	http.HandleFunc("/greeting", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "POST" {
+			body := make(map[string]string)
+			d := json.NewDecoder(r.Body)
+
+			err := d.Decode(&body)
+			if err != nil {
+				fmt.Fprint(w, "error")
+			}
+
+			fmt.Fprintf(w, "hi %v\n", body["name"])
+		}
 	})
 
 	http.ListenAndServe(":8000", nil)
