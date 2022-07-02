6e7543d567d59d586a21d196707671906def01ff
nardiyansah <janatri1398@gmail.com>
2022-07-02 16:20:47 +0700

    add middleware

--- curl/main.go
+++ curl/main.go
@@ -6,25 +6,49 @@ import (
 	"net/http"
 )
 
+func middleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Header.Get("X-key") != "secret" {
+			w.Write([]byte("request denied\n"))
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "hello world\n")
+}
+
+func greeting(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		body := make(map[string]string)
+		d := json.NewDecoder(r.Body)
+
+		err := d.Decode(&body)
+		if err != nil {
+			fmt.Fprint(w, "error")
+		}
+
+		fmt.Fprintf(w, "hi %v\n", body["name"])
+	}
+}
+
 func main() {
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "hello world\n")
-	})
+	mux := http.DefaultServeMux
 
-	http.HandleFunc("/greeting", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == "POST" {
-			body := make(map[string]string)
-			d := json.NewDecoder(r.Body)
+	mux.HandleFunc("/", hello)
+	mux.HandleFunc("/greeting", greeting)
 
-			err := d.Decode(&body)
-			if err != nil {
-				fmt.Fprint(w, "error")
-			}
+	var handler http.Handler = mux
+	handler = middleware(handler)
 
-			fmt.Fprintf(w, "hi %v\n", body["name"])
-		}
-	})
+	server := new(http.Server)
+	server.Addr = ":8000"
+	server.Handler = handler
 
-	http.ListenAndServe(":8000", nil)
+	fmt.Printf("server listen and serve at %v\n", server.Addr)
+	server.ListenAndServe()
 }
